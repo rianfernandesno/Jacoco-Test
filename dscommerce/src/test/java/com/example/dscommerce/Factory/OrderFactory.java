50f9fad7382b319f8da4a45766e24070a2bd8a8e
rianfernandesno <zrpapz12@gmail.com>
2025-05-19 14:05:58 -0300

    OrderServiceTests

---
+++ dscommerce/src/test/java/com/example/dscommerce/Factory/OrderFactory.java
@@ -0,0 +1,18 @@
+package com.example.dscommerce.Factory;
+
+import com.example.dscommerce.entities.*;
+
+import java.time.Instant;
+
+public class OrderFactory {
+
+    public  static Order createOrder(User client){
+        Order order = new Order(1L, Instant.now(), OrderStatus.WAITING_PAYMENT, client, new Payment());
+        Product product = ProductFactory.createProduct();
+
+        OrderItem orderItem = new OrderItem(order, product, 2, 10.0);
+        order.getItems().add(orderItem);
+
+        return order;
+    }
+}
