fa1d7a7030ca58f9cd484a9c7b2960b8222548f2
rianfernandesno <zrpapz12@gmail.com>
2025-05-19 12:40:32 -0300

    AuthServiceTest

---
+++ dscommerce/src/test/java/com/example/dscommerce/services/AuthServiceTest.java
@@ -0,0 +1,66 @@
+package com.example.dscommerce.services;
+
+import com.example.dscommerce.Factory.UserFactory;
+import com.example.dscommerce.entities.User;
+import com.example.dscommerce.services.exceptions.ForbiddenException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class AuthServiceTest {
+
+    @InjectMocks
+    private AuthService service;
+
+    @Mock
+    private UserService userService;
+
+    private User admin, selfClient, otherClient;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        admin = UserFactory.createAdminUser();
+        selfClient = UserFactory.createCustomClientUser(1L, "Bob");
+        otherClient = UserFactory.createCustomClientUser(2L, "Ana");
+
+    }
+
+    @Test
+    public void validateSelfOrAdminShouldDoNothingWhenAdminLogged(){
+        Mockito.when(userService.authenticated()).thenReturn(admin);
+
+        Long userId = admin.getId();
+
+        Assertions.assertDoesNotThrow(()->{
+            service.validateSelfOrAdmin(userId);
+        });
+    }
+
+    @Test
+    public void validateSelfOrAdminShouldDoNothingWhenSelfLogged(){
+        Mockito.when(userService.authenticated()).thenReturn(selfClient);
+
+        Long userId = selfClient.getId();
+
+        Assertions.assertDoesNotThrow(()->{
+            service.validateSelfOrAdmin(userId);
+        });
+    }
+
+    @Test
+    public void validateSelfOrAdminThrowForbiddenExceptionWhenClientOtherLogged(){
+        Mockito.when(userService.authenticated()).thenReturn(selfClient);
+
+        Long userId = otherClient.getId();
+
+        Assertions.assertThrows(ForbiddenException.class,()->{
+            service.validateSelfOrAdmin(userId);
+        });
+    }
+}
